6ddd2d42d787f4e7529b62c85d89e949e34a0067
Michael Basmanov <puglet5@mail.ru>
2022-09-16 21:09:31 +0300

    add name field to users, tweak model, add rolify

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,32 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  name                   :string           default(""), not null
+#
 class User < ApplicationRecord
+  rolify
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, format: URI::MailTo::EMAIL_REGEXP
+  validates :password, confirmation: true
+  validates :name, :email, presence: true
+
+  def author?(obj)
+    obj.user == self
+  end
 end
