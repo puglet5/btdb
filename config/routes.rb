a016c002c6785670d92c3b44bc32626d476539f8
Michael Basmanov <puglet5@mail.ru>
2022-09-16 23:47:08 +0300

    add user controller with show action

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@
 Rails.application.routes.draw do
   root 'pages#home'
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+  resources :users, only: ['show']
 end
