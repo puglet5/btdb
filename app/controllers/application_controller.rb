a84fcdcded22847063de30285b99c9b365e2842c
Michael Basmanov <puglet5@mail.ru>
2022-09-16 22:00:37 +0300

    add nav, fix devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    update_attrs = %i[name email password password_confirmation
+                      current_password]
+    devise_parameter_sanitizer.permit :account_update, keys: update_attrs
+    devise_parameter_sanitizer.permit :sign_up, keys: update_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: %i[login password]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
   root 'pages#home'
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
 end
