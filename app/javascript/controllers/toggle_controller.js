369e696c6930bfb7b488c258d9c394237182ed9c
Michael Basmanov <puglet5@mail.ru>
2022-09-18 19:16:28 +0300

    style sidebar

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -1,9 +1,10 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["toggle"]
+  static targets = ["toggle", "svg"]
 
   toggle() {
     this.toggleTarget.classList.toggle("hidden")
+    this.svgTarget.classList.toggle("rotate-180")
   }
 }
