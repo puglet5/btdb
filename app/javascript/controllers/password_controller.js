64335e25ec1b5b25dfe158e504ef2ca70076bb30
Michael Basmanov <puglet5@mail.ru>
2022-09-18 16:47:07 +0300

    login form misc styling

--- app/javascript/controllers/password_controller.js
+++ app/javascript/controllers/password_controller.js
@@ -2,11 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
 
-  static targets = ["input"]
+  static targets = ["input", "svg"]
 
   toggle() {
     const input = this.inputTarget
 
     input.type = input.type === "password" ? "text" : "password"
+
+    this.svgTarget.classList.toggle("!text-lime-700")
   }
 }
