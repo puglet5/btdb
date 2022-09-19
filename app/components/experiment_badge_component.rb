2d80dc904aec60811c0b3820d9e0c5be37a85a98
Michael Basmanov <puglet5@mail.ru>
2022-09-20 01:24:47 +0300

    refactor case when to hashes

--- app/components/experiment_badge_component.rb
+++ app/components/experiment_badge_component.rb
@@ -1,21 +1,21 @@
 # frozen_string_literal: true
 
 class ExperimentBadgeComponent < ViewComponent::Base
+  attr_reader :status
+
   def initialize(status: '', cls: '')
     @status = status
     @cls = cls
   end
 
-  def color
-    case @status
-    when 'finished'
-      'text-lime-800 bg-lime-100'
-    when 'ongoing'
-      'text-sky-800 bg-sky-100'
-    when 'cancelled'
-      'text-amber-800 bg-amber-100'
-    when 'not_set'
-      'text-gray-800 bg-gray-100'
-    end
+  STATUSES = {
+    'finished' => 'text-lime-800 bg-lime-100',
+    'ongoing' => 'text-sky-800 bg-sky-100',
+    'cancelled' => 'text-amber-800 bg-amber-100',
+    'not_set' => 'text-gray-800 bg-gray-100'
+  }.freeze
+
+  def color(status)
+    STATUSES[status]
   end
 end
--- app/components/flash_component.rb
+++ app/components/flash_component.rb
@@ -1,22 +1,22 @@
 # frozen_string_literal: true
 
 class FlashComponent < ViewComponent::Base
+  attr_accessor :type
+
   def initialize(type: '', msg: '', cls: '')
     @cls = cls
     @type = type
     @msg = msg
   end
 
-  def color
-    case @type
-    when 'error'
-      'text-red-700 border-red-600/50 bg-red-50'
-    when 'alert'
-      'text-amber-700 border-amber-600/50 bg-amber-50'
-    when 'success'
-      'text-lime-700 border-lime-600/50 bg-lime-50'
-    when 'notice'
-      'text-sky-700 border-sky-600/50 bg-sky-50'
-    end
+  TYPES = {
+    'error' => 'text-red-700 border-red-600/50 bg-red-50',
+    'alert' => 'text-amber-700 border-amber-600/50 bg-amber-50',
+    'success' => 'text-lime-700 border-lime-600/50 bg-lime-50',
+    'notice' => 'text-sky-700 border-sky-600/50 bg-sky-50'
+  }.freeze
+
+  def color(type)
+    TYPES[type]
   end
 end
