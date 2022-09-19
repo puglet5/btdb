bb9d332d51847b7480a90f3d57ff304f6c6211d9
Michael Basmanov <puglet5@mail.ru>
2022-09-20 01:11:19 +0300

    reannotate models

--- spec/factories/experiments.rb
+++ spec/factories/experiments.rb
@@ -8,13 +8,14 @@
 #  title      :string           default(""), not null
 #  author     :string           default(""), not null
 #  staff      :string           default(""), not null
-#  category   :integer          default("undef")
-#  status     :integer          default("undef")
+#  category   :integer          default("not_set")
+#  status     :integer          default("not_set")
 #  open_date  :date
 #  close_date :date
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  user_id    :bigint
+#  metadata   :jsonb            not null
 #
 FactoryBot.define do
   factory :experiment do
