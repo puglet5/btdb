80ab3e89bd4365de43c1458fc425f12cc85dbf30
Michael Basmanov <puglet5@mail.ru>
2022-09-19 16:15:49 +0300

    add custom turbo confirm modal

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,3 +6,18 @@ import "@rails/actiontext"
 import "jquery"
 import "@client-side-validations/client-side-validations/src"
 import "@client-side-validations/simple-form/src"
+
+// eslint-disable-next-line no-undef
+Turbo.setConfirmMethod(() => {
+  let modal = document.getElementById("confirm-modal")
+  modal.showModal()
+
+  console.log(modal)
+
+  // eslint-disable-next-line no-unused-vars
+  return new Promise((resolve, reject) => {
+    modal.addEventListener("close", () => {
+      resolve(modal.returnValue == "confirm")
+    }, { once: true })
+  })
+})
