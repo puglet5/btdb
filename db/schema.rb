63c4cff5103235606224c7f429f001913a867f0e
Michael Basmanov <puglet5@mail.ru>
2022-09-22 20:45:51 +0300

    add survey date field to samples

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_22_172925) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_22_174448) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -93,6 +93,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_22_172925) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.jsonb "metadata", default: "{}", null: false
+    t.datetime "survey_date"
     t.index ["metadata"], name: "index_samples_on_metadata", using: :gin
   end
 
--- spec/factories/samples.rb
+++ spec/factories/samples.rb
@@ -4,13 +4,14 @@
 #
 # Table name: samples
 #
-#  id         :bigint           not null, primary key
-#  title      :string
-#  category   :integer          default("not_set"), not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  user_id    :integer
-#  metadata   :jsonb            not null
+#  id          :bigint           not null, primary key
+#  title       :string
+#  category    :integer          default("not_set"), not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :integer
+#  metadata    :jsonb            not null
+#  survey_date :datetime
 #
 FactoryBot.define do
   factory :sample do
