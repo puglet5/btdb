6ffc9566fceeae320ab37633e6afc415a5cc6589
Michael Basmanov <puglet5@mail.ru>
2022-09-25 19:56:38 +0300

    handle 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@
 class ApplicationController < ActionController::Base
   include ActiveStorage::SetCurrent
   include Authorization
+  include ErrorHandling
 
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
