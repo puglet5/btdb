73e5217262489e823aba50ed24d5b0c6e3d54ab4
Michael Basmanov <puglet5@mail.ru>
2022-09-28 00:10:36 +0300

    create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,47 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+require 'database_cleaner/active_record'
+
+DatabaseCleaner.clean_with(:truncation)
+
+10.times do
+  User.create!(name: Faker::Name.name,
+               email: Faker::Internet.email,
+               password: '123456')
+end
+
+admin = User.create!(name: 'Michael Basmanov',
+                     email: 'test@test.test',
+                     password: '123456')
+
+admin.add_role(:admin)
+
+users = User.all
+users.each do |user|
+  title = Faker::Lorem.sentence
+  author = Faker::Name.name
+  description = Faker::Lorem.paragraph
+  status = Experiment.statuses.keys.sample
+  category = Experiment.categories.keys.sample
+  metadata = Faker::Json.shallow_json(width: 3,
+                                      options: { key: 'Science.element',
+                                                 value: 'Number.decimal(l_digits: 3, r_digits: 3)' })
+  experiment = Experiment.create!(title: title, description: description, status: status, category: category, user_id: user.id, author: author)
+  experiment.update!(metadata: metadata)
+
+  title = Faker::Lorem.sentence
+  description = Faker::Lorem.paragraph
+  category = Sample.categories.keys.sample
+  metadata = Faker::Json.shallow_json(width: 3,
+                                      options: { key: 'Science.element',
+                                                 value: 'Number.decimal(l_digits: 3, r_digits: 3)' })
+  up = Sample.create!(title: title, description: description, category: category, user_id: user.id, experiment_ids: [[nil, Experiment.all.sample.id].sample])
+  up.update!(metadata: metadata)
+
+  title = Faker::Lorem.sentence
+  equipment = Faker::Lorem.sentence
+  date = Date.today - rand(10_000)
+  category = Measurment.categories.keys.sample
+  up = Measurment.create!(title: title, date: date, equipment: equipment, category: category, user_id: user.id, sample_id: Sample.all.sample.id)
+end
