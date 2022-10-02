6235f54f62e033e9ed42c2cb1a760dcd724c7554
Michael Basmanov <puglet5@mail.ru>
2022-10-03 00:37:17 +0300

    add spectra policy

---
+++ app/policies/spectrum_policy.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class SamplePolicy < ApplicationPolicy
+  def create?
+    true
+  end
+
+  def update?
+    policy(record.measurment).update?
+  end
+
+  def destroy?
+    policy(record.measurment).destroy?
+  end
+
+  def index?
+    true
+  end
+
+  def show?
+    true
+  end
+end
