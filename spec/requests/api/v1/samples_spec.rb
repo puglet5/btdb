cb57821d69d8040790ccc42b01180078e42c15fc
Michael Basmanov <puglet5@mail.ru>
2023-01-21 18:14:30 +0300

    update samples spec

--- spec/requests/api/v1/samples_spec.rb
+++ spec/requests/api/v1/samples_spec.rb
@@ -2,10 +2,20 @@
 
 require 'swagger_helper'
 
-RSpec.describe Api::V1::SamplesController do
+RSpec.fdescribe Api::V1::SamplesController do
   before(:each) do
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
+  end
+
+  let(:application) { FactoryBot.create(:application) }
+  let(:user)        { FactoryBot.create(:user) }
+  let(:sample)      { FactoryBot.create(:sample, user: user) }
+  let(:token)       { FactoryBot.create(:access_token, application: application, resource_owner_id: user.id) }
+
+  before(:each) do
+    @serializer = SampleSerializer.new(sample)
+    @serialization = ActiveModelSerializers::Adapter.create(@serializer)
   end
 
   describe 'GET #index' do
@@ -17,16 +27,32 @@ RSpec.describe Api::V1::SamplesController do
     end
 
     context 'when authorized' do
-      let(:application) { FactoryBot.create(:application) }
-      let(:user)        { FactoryBot.create(:user) }
-      let(:sample) { FactoryBot.create(:sample, user: user) }
-      let(:token) { FactoryBot.create(:access_token, application: application, resource_owner_id: user.id) }
+      subject { JSON.parse(@serialization.to_json) }
 
       it 'succeeds' do
         sample.save!
         get '/api/v1/samples', params: {}, headers: { Authorization: "Bearer #{token.token}" }
         expect(response).to be_successful
-        expect(JSON.parse(response.body).first).to eq(JSON.parse(sample.to_json))
+        expect(subject).to eq(JSON.parse(response.body).first)
+      end
+    end
+  end
+
+  describe 'GET #show' do
+    context 'when unauthorized' do
+      it 'fails with HTTP 401' do
+        get '/api/v1/samples/1'
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+
+    context 'when authorized' do
+      subject { JSON.parse(@serialization.to_json) }
+
+      it 'succeeds' do
+        get "/api/v1/samples/#{sample.id}", params: {}, headers: { Authorization: "Bearer #{token.token}" }
+        expect(response).to be_successful
+        expect(subject).to eq(JSON.parse(response.body))
       end
     end
   end
