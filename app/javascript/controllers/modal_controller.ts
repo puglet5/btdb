166968eca0c1fecaa95173b54fe14cc5c0798676
Michael Basmanov <puglet5@mail.ru>
2023-12-05 15:51:51 +0300

    feat[ui]: close modal on esc

--- app/javascript/controllers/modal_controller.ts
+++ app/javascript/controllers/modal_controller.ts
@@ -1,4 +1,5 @@
 import { Controller } from "@hotwired/stimulus"
+import hotkeys from "hotkeys-js"
 import { Typed } from "stimulus-typescript"
 
 const targets = {
@@ -7,9 +8,24 @@ const targets = {
 
 export default class extends Typed(Controller, { targets }) {
 
-  static targets = ["modal"]
+  modal: HTMLDialogElement = document.getElementById("confirm-modal")! as HTMLDialogElement
+
+  connect() {
+    // @ts-ignore
+    hotkeys("escape", (event) => {
+      event.preventDefault()
+      this.close()
+    })
+  }
 
   toggle() {
     this.modalTarget.classList.toggle("hidden")
+  }
+
+  close() {
+    this.modalTarget.classList.add("hidden")
+    if (this.modal) {
+      this.modal.close("cancel")
+    }
   }
 }
